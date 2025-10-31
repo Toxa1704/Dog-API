64772730440c1b5c324c0d013bcd4e11eaab0235
Toxa1704 <Toxa1704@outlook.com>
2025-10-31 15:37:34 +0200

    main

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ const img = document.getElementById('dog-img');
 const loader = document.querySelector('.loader');
 const errorMsg = document.querySelector('.error');
 
+async function getDog() {
+    loader.style.display = 'block';
+    errorMsg.textContent = '';
+    img.src = '';
+
+    try {
+        const res = await fetch('https://dog.ceo/api/breeds/image/random');
+        console.log(res);
+        if (!res.ok) throw new Error('Network error');
+
+        const data = await res.json();
+        img.src = data.message;
+    } catch (error) {
+        errorMsg.textContent = 'Oops! Try again 🐾';
+    }finally{
+        loader.style.display = 'none';
+    }
+
+}
+
+getDog();
